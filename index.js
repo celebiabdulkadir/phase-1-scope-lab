35adde73064951582ea54eed585af94a756c1e33
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-05-11 17:38:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+  return (customerName = customerName.toUpperCase());
+}
+var bestCustomer;
+
+function setBestCustomer() {
+  // Assign a value to the global variable
+  bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+  // Overwrite the global variable
+  bestCustomer = "maybe bob";
+}
+// Declare a constant in global scope
+const leastFavoriteCustomer = "bob";
+
+// Function that attempts to change the constant
+function changeLeastFavoriteCustomer() {
+  // This will throw an error
+  leastFavoriteCustomer = "not bob";
+}
